1851bdb34533863a63ca27e1ec67a6574ffb3519
Martin Schroeder <m.schroeder@tu-berlin.de>
2018-06-28 11:20:50 +0200

    update to 0.7.0rc6
    0.7.0rc6
    - fixes of AppConfig for setup.py develop

--- pyscada/bacnet/apps.py
+++ pyscada/bacnet/apps.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
@@ -8,3 +9,4 @@ from django.utils.translation import ugettext_lazy as _
 class PyScadaBACnetConfig(AppConfig):
     name = 'pyscada.bacnet'
     verbose_name = _("PyScada BACnet Master")
+    path = os.path.dirname(os.path.realpath(__file__))
