bc4b0f17c3daf2ec35f292dc741a73da9f7b1c7d
clavay <clavayssiere@univ-pau.fr>
2023-04-05 11:41:24 +0200

    add project informations
    use project informations
    update license and pyscada version

--- pyscada/bacnet/__init__.py
+++ pyscada/bacnet/__init__.py
@@ -2,13 +2,16 @@
 from __future__ import unicode_literals
 
 
-__version__ = '0.7.1rc1'
-__author__ = 'Martin Schröder'
+__version__ = "0.8.0"
+__author__ = "Martin Schröder, Camille Lavayssiere"
+__email__ = "team@pyscada.org"
+__description__ = "BACnet extension for PyScada a Python and Django based Open Source SCADA System"
+__app_name__ = "BACnet"
 
 PROTOCOL_ID = 4
 
 parent_process_list = [{'pk': PROTOCOL_ID,
-                        'label': 'pyscada.bacnet',
-                        'process_class': 'pyscada.bacnet.worker.Process',
+                        'label': 'pyscada.' + __app_name__.lower(),
+                        'process_class': 'pyscada.' + __app_name__.lower() + '.worker.Process',
                         'process_class_kwargs': '{"dt_set":30}',
                         'enabled': True}]
