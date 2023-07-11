9ba486ac6c8b1247e41486d80f356fb34f836575
clavay <clavayssiere@univ-pau.fr>
2023-07-11 09:38:09 +0200

    replace setuptools find_packages by find_namespace_packages

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 import os
 from pyscada import bacnet as pyscada_app
 
@@ -35,7 +35,7 @@ setup(
         'bacpypes',
         'BAC0',
     ],
-    packages=find_packages(exclude=["project", "project.*"]),
+    packages=find_namespace_packages(exclude=["project", "project.*"]),
     include_package_data=True,
     zip_safe=False,
     test_suite='runtests.main'
